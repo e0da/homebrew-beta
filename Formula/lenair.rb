cb42a5207af57b0ca147180b3b0e1a98ef22219d
Justin Force <justin.force@gmail.com>
2026-08-31 02:05:11 -0600

    Revert Gate 4 throwaway version-spacing corruption

--- Formula/lenair.rb
+++ Formula/lenair.rb
@@ -5,7 +5,7 @@ class Lenair < Formula
   desc "Agent-first Linear CLI"
   homepage "https://github.com/e0da/lenair"
   url "https://github.com/e0da/homebrew-beta/releases/download/v0.1.0/lenair-darwin-arm64-v0.1.0-lenair.tar.gz"
-  version  "0.1.0"
+  version "0.1.0"
   sha256 "27b5b86c7172235a2a13952c7f13ef74b5d573357928233431a7fe9236612767"
   license "MIT"
 
