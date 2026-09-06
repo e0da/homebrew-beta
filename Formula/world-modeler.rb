fc897bc8e5cf1a3cee846f6a238a482fc2a982d5
Justin Force <justin.force@gmail.com>
2026-09-06 03:16:19 -0600

    v1.4.0: bump url/version/sha256

--- Formula/world-modeler.rb
+++ Formula/world-modeler.rb
@@ -4,9 +4,9 @@
 class WorldModeler < Formula
   desc "Portable capability-based runtime for JSON and JSONL world models"
   homepage "https://github.com/e0da/world-modeler"
-  url "https://github.com/e0da/homebrew-beta/releases/download/world-modeler-v1.3.0/world-modeler-darwin-arm64-v1.3.0-world-modeler.tar.gz"
-  version "1.3.0"
-  sha256 "d33f93d42608f76131b5ad53dbf88b616bece1ea92f31edee39ec2b5a17859ad"
+  url "https://github.com/e0da/homebrew-beta/releases/download/world-modeler-v1.4.0/world-modeler-darwin-arm64-v1.4.0-world-modeler.tar.gz"
+  version "1.4.0"
+  sha256 "2c544b2c4f37f7d01368c7a7c3f0bf344fa18df9abe8c6c9785456dab75ee0e6"
   license "MIT"
 
   depends_on arch: :arm64
